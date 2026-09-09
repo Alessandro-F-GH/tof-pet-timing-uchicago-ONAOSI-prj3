1fcbc307cb64e63680cfdb21c22b86ad2eec0ed1
Alessandro-F-GH <alessandro.falcetta02@gmail.com>
2026-09-09 12:42:49 -0500

    Remove bootstrap CTR export

--- utils_fit/__init__.py
+++ utils_fit/__init__.py
@@ -9,7 +9,7 @@ from .gaussian import (
     fit_delta_times_ps,
     scan_timing_grid,
 )
-from .ctr import bootstrap_ctr_ps, fit_ctr_ps
+from .ctr import fit_ctr_ps
 from .io import load_fit_csv, write_fit_csv
 from .outliers import RobustOutlierResult, robust_mad_filter
 
@@ -26,7 +26,6 @@ __all__ = [
     "fit_delta_times_ps",
     "scan_timing_grid",
     "fit_ctr_ps",
-    "bootstrap_ctr_ps",
     "load_fit_csv",
     "write_fit_csv",
 ]
