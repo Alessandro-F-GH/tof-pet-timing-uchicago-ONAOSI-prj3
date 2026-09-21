ccb519495d1be0d23011316ab48e48bc21659cb9
Alessandro-F-GH <alessandro.falcetta02@gmail.com>
2026-09-21 20:05:40 +0200

    Register locally connected MLP

--- waveform_analysis/ml_pipeline/models/registry.py
+++ waveform_analysis/ml_pipeline/models/registry.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
+from .locally_connected_mlp import MODEL_SPEC as LOCALLY_CONNECTED_MLP_SPEC
 from .mlp import MODEL_SPEC as MLP_SPEC
 from .onishi_cnn import MODEL_SPEC as ONISHI_CNN_SPEC
 from .spec import ModelSpec
 
 _REGISTRY: dict[str, ModelSpec] = {
     MLP_SPEC.name: MLP_SPEC,
+    LOCALLY_CONNECTED_MLP_SPEC.name: LOCALLY_CONNECTED_MLP_SPEC,
     ONISHI_CNN_SPEC.name: ONISHI_CNN_SPEC,
 }
 
