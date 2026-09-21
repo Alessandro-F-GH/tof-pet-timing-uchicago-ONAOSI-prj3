5e038d65ae70b4b11cb274c0dcdf8b7423004bdc
Alessandro-F-GH <alessandro.falcetta02@gmail.com>
2026-09-21 20:06:29 +0200

    Test locally connected MLP registry entry

--- waveform_analysis/tests/test_registry.py
+++ waveform_analysis/tests/test_registry.py
@@ -5,7 +5,7 @@ from waveform_analysis.ml_pipeline.models import model_names
 
 class RegistryTests(unittest.TestCase):
     def test_active_registry(self):
-        self.assertEqual(set(model_names()), {"mlp", "onishi_cnn"})
+        self.assertEqual(set(model_names()), {"mlp", "locally_connected_mlp", "onishi_cnn"})
 
 
 if __name__ == "__main__":
